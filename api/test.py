a51bbfb496b02f23dda31fd9b8eb4b20b2888536
Igor Morozov <gadson@yandex.ru>
2019-09-17 14:36:37 +0300

    Update test.py

--- api/test.py
+++ api/test.py
@@ -15,4 +15,4 @@ def push_to_fcm(device_token, gcm_key, title=None, message=None, data=None, soun
     )
 
 
-push_to_fcm("ePpquqf0Ew0:APA91bHZ--eFfFdg4SMAmmXliCa3gT5obLTy2zm3D4mnUTQ9n2GkG_--14sDaDtWg_jz7K18_yICUfSYMl4gWjB9TvRG1tz7-LOO4oNEbTHAvoaVADeejJV2VYproEnNOyXZE_iFj-KI", "AAAAqxOQx7w:APA91bERR9A5_nnk0tdm6fdiswEDi0KLpQyAWC2zrGoYR2LPHpUGCPg-lNlzcVoVicE8TpgvYncBwrP8dYprvLNRJlZPI1iyOAfmib7L7B4A5yUuJ0h0kmpcHSu-Z_pzWc0EMAOqdPWr", "test Title", "messagebody")
+push_to_fcm("", "", "test Title", "messagebody")
